29a93af37e59f65d4e60c3d6411d4edaf3288d17
danieleborgo <daniele.borgo@mail.polimi.it>
2021-12-22 18:07:33 +0100

    Initial commit

---
+++ octoprint_gpiostatus/static/js/gpiostatus.js
@@ -0,0 +1,29 @@
+/*
+ * View model for GPIOStatus
+ *
+ * Author: Daniele Borgo
+ * License: GPLv3
+ */
+$(function() {
+    function GpiostatusViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: GpiostatusViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_gpiostatus, #tab_plugin_gpiostatus, ...
+        elements: [ /* ... */ ]
+    });
+});
